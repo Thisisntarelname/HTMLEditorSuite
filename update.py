caf5148d55220b093d4f65d9364ebba93d1912ca
Fake Name <80899773+Thisisntarelname@users.noreply.github.com>
2022-03-17 17:05:40 +0000

    Update is kind of almost working now, loop goes on foreever

--- update.py
+++ update.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 #This takes the xlsx file from /uploads, edits the /Data pages to reflect upadted information from xlsx file. 
+#pip3 install beautifulsoup4
+#pip3 install lxml
 import os
 import openpyxl
 import glob
@@ -43,41 +45,64 @@ while i <= max_rows:
         teachernames.append(teacherName)
         filenames.append(searchThis)
 
-        #this is where we will upadte html with new information
-        #open filename and read
-        #paste tmplate
-        
-        
-        with open('test.html', 'r') as f:
-        
-            contents = f.read()
-        
-            soup = BeautifulSoup(contents, 'lxml')
-        
-            #---------Subejcts
-            tag = (soup.find(id='subjects'))#Finds h3 with id: subjects
-            subjects = ["English", "Math", "Science", "Social Studies"]#list of subjects created from xlsx file
-            build_subjects = subjects[0]#Starting the loop off with first item
-            i = 1
-            while i<len(subjects):#couldn't get a for loop to work
-              print(i)
-              build_subjects = build_subjects+", "+subjects[i]#Concated each item to variable passed form last loop through
-              print(build_subjects)
-              i = i+1
-        
-            subject_string = '<h3 id="subject">Subjects: '+build_subjects+'</h3>'
-            print(subject_string)
-            tag.replace_with(subject_string)
-            #---------Classes
-            #---------Rooms
-        
+        #This is where we open the html file and edit it
+        os.chdir("..")
+        with open('TeacherTemplate', 'r') as f:
+      
+          contents = f.read()
           
+      
+          soup = BeautifulSoup(contents, 'lxml')
+
           
-        with open("example_modified.html", "w") as f_output:
-        
-            f_output.write(soup.prettify(formatter=None)) 
-        
-        
+        os.chdir('Data')
+
+        #---------Subejcts
+        tag = (soup.find(id='subjects'))#Finds h3 with id: subjects
+        print(tag)
+        subjects = ["English", "Math", "Science", "Social Studies"]#list of subjects created from xlsx file
+        build_subjects = subjects[0]#Starting the loop off with first item
+        i = 1
+        while i<len(subjects):#couldn't get a for loop to work
+          build_subjects = build_subjects+", "+subjects[i]#Concated each item to variable passed form last loop through
+          i = i+1
+    
+        subject_string = '<h3 id="subject">Subjects: '+build_subjects+'</h3>'
+        tag.replace_with(subject_string)
+        #---------Classes
+    
+        tag = (soup.find(id='classes'))
+        classes = ["IB English", "Algebra 1"]
+        build_classes = classes[0]
+        i = 1
+        while i<len(classes):
+          build_classes = build_classes+", "+classes[i]
+          i = i+1
+    
+        class_string = '<h3 id="classes">Classes: '+build_classes+'</h3>'
+        tag.replace_with(class_string)
+        #---------Rooms
+    
+        tag = (soup.find(id='rooms'))
+        rooms = ["201", "302"]#Keep room numbers in string, integers would prpbably require some tweaking of the code
+        build_rooms = rooms[0]
+        i = 1
+        while i<len(rooms):
+          build_rooms = build_rooms+", "+rooms[i]
+          i = i+1
+    
+        room_string = '<h3 id="rooms">Rooms: '+build_rooms+'</h3>'
+        tag.replace_with(room_string)
+      
+      
+        with open(searchThis, "w") as f_output:
+    
+          f_output.write(soup.prettify(formatter=None)) 
+    
+      
+
+
+
 
 
 
